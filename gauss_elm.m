1ed0da6a952126d99ab51982947309625e3c9602
amirbaharvand66 <amirbaharvand66@yahoo.com>
2020-10-09 23:13:56 +0330

    bug fixed in upper triangle formation loop

--- gauss_elm.m
+++ gauss_elm.m
@@ -22,9 +22,9 @@ n = size(k, 1);
 
 % transorming k to an upper triangle
 for ii = 1:n - 1
-    for jj = 1:(n - ii - 1) % iterate over rows
-        piv = -k(jj + ii, ii) / k(ii, ii); % pivot value
-        k(jj + ii, :) = piv * k(ii, :) + k(jj + ii, :);
+    for jj = 1:(n - ii) % iterate over rows
+        piv = k(jj + ii, ii) / k(ii, ii); % pivot value
+        k(jj + ii, :) = -piv * k(ii, :) + k(jj + ii, :);
     end
 end
 
@@ -32,7 +32,7 @@ end
 d(n) = k(n, n + 1) / k(n, n);
 
 % backward substition
-for ii = (n - 1):-1:1 % moving substition in rows
+for ii = (n - 1):-1:1 % substition in rows
     a = k(ii, n + 1); % creating an intermediate variable instead of p
     for jj = (ii + 1):n % p assembly
         a = a - k(ii, jj) * d(jj);
